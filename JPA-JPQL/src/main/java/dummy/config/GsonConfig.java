5777869067ebb8342005932a226570bdb1440fc1
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-03-18 21:04:00 +0530

    na

--- JPA-JPQL/src/main/java/dummy/config/GsonConfig.java
+++ JPA-JPQL/src/main/java/dummy/config/GsonConfig.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import org.springframework.boot.json.JsonParseException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -25,39 +24,36 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 import com.google.gson.internal.bind.DateTypeAdapter;
 import com.google.gson.internal.bind.SqlDateTypeAdapter;
 
 @Configuration
 public class GsonConfig implements WebMvcConfigurer{
 
-	@Bean
-	public Gson gson() {
-	    GsonBuilder b = new GsonBuilder();
-	  //  b.registerTypeAdapterFactory(HibernateProxyTypeAdapter.FACTORY);
-	    b.registerTypeAdapterFactory(DateTypeAdapter.FACTORY);
-	 //   b.registerTypeAdapterFactory(TimestampTypeAdapter.FACTORY);
-	    b.registerTypeAdapterFactory(SqlDateTypeAdapter.FACTORY);
-	 //   b.registerTypeAdapterFactory(LocalDateTimeTypeAdapter.FACTORY); 
-	    b.registerTypeAdapter(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
-	        @Override
-	        public LocalDateTime deserialize(JsonElement json, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-	            Instant instant = Instant.ofEpochMilli(json.getAsJsonPrimitive().getAsLong());
-	            return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
-	        }
-	    });
-	    b.registerTypeAdapter(LocalDate.class, new JsonDeserializer<LocalDate>() {
-	    	@Override
-	    		public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-	    	        return LocalDate.parse(json.getAsJsonPrimitive().getAsString());
-	    	}
-	    });
-	    b.registerTypeAdapter(LocalDate.class, new LocalDateAdapter());
-	    return b.create();
+	@Bean 
+	public Gson gson() { 
+	    return new GsonBuilder().registerTypeAdapterFactory(DateTypeAdapter.FACTORY)
+				    .registerTypeAdapterFactory(SqlDateTypeAdapter.FACTORY)
+				    .registerTypeAdapter(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
+					@Override
+					public LocalDateTime deserialize(JsonElement json, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+					    Instant instant = Instant.ofEpochMilli(json.getAsJsonPrimitive().getAsLong());
+					    return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+					}
+				    })
+				   .registerTypeAdapter(LocalDate.class, new JsonDeserializer<LocalDate>() {
+					@Override
+						public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+						return LocalDate.parse(json.getAsJsonPrimitive().getAsString());
+					}
+				    })
+				.registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+				.create();
 	}
 
 	@Override
-	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) { 
 	    StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
 	    stringConverter.setWriteAcceptCharset(false);
 	    stringConverter.setSupportedMediaTypes(Collections
