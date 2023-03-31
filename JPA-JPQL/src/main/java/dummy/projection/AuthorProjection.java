14560019f31b494aef63ef0c9ebe0e1fb391eb95
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-03-31 12:05:57 +0530

    Update AuthorProjection.java

--- JPA-JPQL/src/main/java/dummy/projection/AuthorProjection.java
+++ JPA-JPQL/src/main/java/dummy/projection/AuthorProjection.java
@@ -3,7 +3,7 @@ package dummy.projection;
 //used as a Convertor from extracting managed Entity via Repository to a JPA Projection
 public interface AuthorProjection {
 	
-	Long getAuthorid();
+	Long getAuthorid(); // primitive is not allowed
 	String getFirstname(); 
 	String getLastname();
 
