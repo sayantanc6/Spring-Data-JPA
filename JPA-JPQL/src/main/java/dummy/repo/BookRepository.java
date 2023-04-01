0c4983ac2d409f5cf9d11d42da21f28093ead1ed
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-04-01 10:48:31 +0530

    Update BookRepository.java

--- JPA-JPQL/src/main/java/dummy/repo/BookRepository.java
+++ JPA-JPQL/src/main/java/dummy/repo/BookRepository.java
@@ -2,7 +2,6 @@ package dummy.repo;
 
 import java.util.List;
 
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,22 +9,13 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import dummy.entity.Book;
-import dummy.projection.BookProjection;
 
 @Repository
-@Transactional
+@Transactional(noRollbackFor = {Exception.class}) 
 public interface BookRepository extends CrudRepository<Book, Long> {
 
-	public void updateAll(List<Book> books,String author,String title,int batchSize);
-	public void insertAllItems(List<?> collections,int batchSize);
+	public void updateAll(List<Book> books,String author,String title,int batchSize);	
 	
-	 /* using naming convention for query alias value depends on get*() method for JPA close Projection
-	  * So whatever naming convention for column name,change it as alias name for all lowercase letters..
-	  * then getter methods for JPA close Projection should be get<first-uppercase-letter><rest-all-lowercase-letters>
-	  */
-	@Query(value = "select b.ISBN as isbn,b.author as author,b.title as title,b.price as price"
-			+ " from Book as b"
-			+ " inner join Author as a"
-			+ " on a.Authorid = :authorID ")
-	public List<BookProjection> findAllBooksgivenauthorID(@Param("authorID")long authorID);
+	@Query(value = "select b from Book as b inner join Author as a on a.Authorid = :authorID ")
+	public List<Book> findAllBooksgivenauthorID(@Param("authorID")long authorID);
 }
