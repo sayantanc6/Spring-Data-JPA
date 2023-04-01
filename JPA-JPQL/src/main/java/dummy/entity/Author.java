12f9af3c14053033c83ff1d03374f837710eb236
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-04-01 10:44:36 +0530

    Update Author.java

--- JPA-JPQL/src/main/java/dummy/entity/Author.java
+++ JPA-JPQL/src/main/java/dummy/entity/Author.java
@@ -5,15 +5,20 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @Entity
@@ -30,8 +35,17 @@ public class Author {
 	
 	private String lastname;
 	
-	@ManyToMany(targetEntity = Book.class,
+	@ManyToMany(targetEntity = Book.class,fetch = FetchType.EAGER,
 			cascade = {CascadeType.PERSIST,CascadeType.MERGE},mappedBy = "authors")
+	// to avoid stackoverflowerror for recursive calls using manytomany
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+	/* to avoid infinite recursion 
+	 * without ignoring getters/setters during serialization.
+	 * It's the forward part of reference.
+	 * Also used in @OneToMany
+	 */
+	@JsonManagedReference
 	public Set<Book> books = new HashSet<>();
 	
 }
