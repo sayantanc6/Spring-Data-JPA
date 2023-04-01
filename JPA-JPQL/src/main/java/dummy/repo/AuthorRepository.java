6a68f99b750da2639a2b8394826d4abf4c2ec176
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-04-01 10:46:31 +0530

    Update AuthorRepository.java

--- JPA-JPQL/src/main/java/dummy/repo/AuthorRepository.java
+++ JPA-JPQL/src/main/java/dummy/repo/AuthorRepository.java
@@ -9,21 +9,13 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import dummy.entity.Author;
-import dummy.projection.AuthorProjection;
 
 @Repository
-@Transactional
+@Transactional(rollbackFor = {Exception.class})
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 
 	void updateAllAuthor(List<Author> authors);
 	
-	/* using naming convention for query alias value depends on get*() method for JPA close Projection
-	  * So whatever naming convention for column name,change it as alias name for all lowercase letters..
-	  * then getter methods for JPA close Projection should be get<first-uppercase-letter><rest-all-lowercase-letters>
-	  */
-	@Query("select a.Authorid as authorid,a.firstname as firstname,a.lastname as lastname"
-			+ " from Author a "
-			+ "inner join Book b "
-			+ "on b.ISBN = :isbn")
-	List<AuthorProjection> findAllAuthorsgivenISBN(@Param("isbn")long isbn); 
+	@Query("select a from Author a inner join Book b on b.ISBN = :isbn")
+	List<Author> findAllAuthorsgivenISBN(@Param("isbn")long isbn); 
 }
