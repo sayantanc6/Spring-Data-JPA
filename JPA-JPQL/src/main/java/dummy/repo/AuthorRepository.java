842956d02a9606d6cf4fe1c594d517e6f52a5be6
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-04-04 14:27:53 +0530

    Update AuthorRepository.java

--- JPA-JPQL/src/main/java/dummy/repo/AuthorRepository.java
+++ JPA-JPQL/src/main/java/dummy/repo/AuthorRepository.java
@@ -13,8 +13,6 @@ import dummy.entity.Author;
 @Repository
 @Transactional(rollbackFor = {Exception.class})
 public interface AuthorRepository extends CrudRepository<Author, Long> {
-
-	void updateAllAuthor(List<Author> authors);
 	
 	@Query("select a from Author a inner join Book b on b.isbn = :isbn")
 	List<Author> findAllAuthorsgivenISBN(@Param("isbn")long isbn); 
