789d21a3f311374e3b1c187dcb03bf64160468a9
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-04-05 13:46:17 +0530

    Update AuthorRepository.java

--- JPA-JPQL/src/main/java/dummy/repo/AuthorRepository.java
+++ JPA-JPQL/src/main/java/dummy/repo/AuthorRepository.java
@@ -2,13 +2,16 @@ package dummy.repo;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import dummy.entity.Author;
+import dummy.projection.BookProjection;
 
 @Repository
 @Transactional(rollbackFor = {Exception.class})
@@ -16,4 +19,9 @@ public interface AuthorRepository extends CrudRepository<Author, Long> {
 	
 	@Query("select a from Author a inner join Book b on b.isbn = :isbn")
 	List<Author> findAllAuthorsgivenISBN(@Param("isbn")long isbn); 
+	
+	@Query("select new dummy.projection.BookProjection(b.isbn,b.author,b.price,b.title)" 
+			     + " from Author as a inner join Book as b"
+			     + " on b.isbn in (?1)")
+	List<BookProjection> findAllAuthorsgivenISBNs(List<Long> isbns);
 }
